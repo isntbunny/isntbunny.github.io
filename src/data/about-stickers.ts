60db7ed8e536c892cb1e2bb2111d5f044a037c97
isntbunny <isntbunny@outlook.com>
2026-07-20 03:30:52 +0000

    about布局、snowflake位置、优化about代码把图片资源存放到ts文件

---
+++ src/data/about-stickers.ts
@@ -0,0 +1,187 @@
+// src/data/about-stickers.ts
+
+// ============ Blinkies ============
+export const blinkies = [
+  'https://i.postimg.cc/mgqq35FP/0178-mikuwink2.gif',
+  'https://i.postimg.cc/tgxcBNhW/1071338ipvpyh23ww.gif',
+  'https://i.postimg.cc/nLDW3GvN/2002.gif',
+  'https://i.postimg.cc/Ssbv3Yrw/alifewhatsthat.gif',
+  'https://i.postimg.cc/FRXB8kpz/badwebsite.gif',
+  'https://i.postimg.cc/gjYBk91t/cantlivwithoutthis.gif',
+  'https://i.postimg.cc/hvS3PFHW/cn-needcoffee.gif',
+  'https://i.postimg.cc/yxvpDQH7/dontplaywell.gif',
+  'https://i.postimg.cc/3rBcfRDn/dreaminpixels.gif',
+  'https://i.postimg.cc/445S2d9C/emails.gif',
+  'https://i.postimg.cc/m2g6vsYg/foodblink5.gif',
+  'https://i.postimg.cc/VLkHx8qj/foodblinkies1.gif',
+  'https://i.postimg.cc/WbYfjH8C/garfieald.gif',
+  'https://i.postimg.cc/T3yNPJNS/gg3.gif',
+  'https://i.postimg.cc/rpxh1Fk1/i-3spongebob.gif',
+  'https://i.postimg.cc/Ss4gDFSv/ilovemypartner.gif',
+  'https://i.postimg.cc/rsmCPqB2/Internet-ruined-me.gif',
+  'https://i.postimg.cc/j5hZ1gCv/lifesfun.gif',
+  'https://i.postimg.cc/nrc0f4Qg/lvtopaint.gif',
+  'https://i.postimg.cc/rsngVD1D/meds.gif',
+  'https://i.postimg.cc/BZMmcVrQ/nap.gif',
+  'https://i.postimg.cc/WbzX97Vd/naughty.gif',
+  'https://i.postimg.cc/dtfngTQr/not-human.gif',
+  'https://i.postimg.cc/fR05sTkF/online.gif',
+  'https://i.postimg.cc/YSKRCKW9/pcneversleeps.gif',
+  'https://i.postimg.cc/1zGHgCG2/self.gif',
+  'https://i.postimg.cc/3R3Z2X97/sleepy.gif',
+  'https://i.postimg.cc/MTzY17tK/snickers.gif',
+  'https://i.postimg.cc/HxTtbw3M/wannaflyaway.gif',
+  'https://i.postimg.cc/rmgNjDYQ/wwf.gif',
+]
+
+// ============ Stamps ============
+export const stamps = [
+  'https://i.postimg.cc/R0S0PCFj/being-childish-stamp-by-creativeness.gif',
+  'https://i.postimg.cc/mrLrdZkL/cautionimweird.png',
+  'https://i.postimg.cc/rpVphysW/cityscape.gif',
+  'https://i.postimg.cc/pdsL3N55/cute-computer-geeks.gif',
+  'https://i.postimg.cc/W451xRqw/ddntsurviv2016.png',
+  'https://i.postimg.cc/LXh8pyjv/drawinmathclas.png',
+  'https://i.postimg.cc/B6tn0h2R/dream.png',
+  'https://i.postimg.cc/qRgvTm8B/error000010.gif',
+  'https://i.postimg.cc/B6yQQxgt/i-love-my-computer-stamp-by-taryn-syndrome.png',
+  'https://i.postimg.cc/HnyWJVZM/i-music-yayy.gif',
+  'https://i.postimg.cc/4y94Km2C/i-suck-at-drawing-hands.png',
+  'https://i.postimg.cc/ThvdnfB8/ilvthenight.gif',
+  'https://i.postimg.cc/y6g0hj2X/loveribbon.gif',
+  'https://i.postimg.cc/y6g0hj2t/madoka2.gif',
+  'https://i.postimg.cc/h4zV8sHg/mc.png',
+  'https://i.postimg.cc/QN21f5rw/miku.gif',
+  'https://i.postimg.cc/Zq5dWxrF/mushroom.png',
+  'https://i.postimg.cc/ZYGpw3SC/no-signal.gif',
+  'https://i.postimg.cc/yY09td1N/old-web-stamp-by-vtge-dcgi6h4.png',
+  'https://i.postimg.cc/3J2pVR84/out-of-order.jpg',
+  'https://i.postimg.cc/J4jJvnrq/robots.gif',
+  'https://i.postimg.cc/CLbDX1h6/slplate.png',
+  'https://i.postimg.cc/SKxMnfWv/squidward.png',
+  'https://i.postimg.cc/jSKNbb23/stayup.gif',
+  'https://i.postimg.cc/NjYTccF3/suck-atlife.gif',
+  'https://i.postimg.cc/K8NTcHgh/takemed.gif',
+  'https://i.postimg.cc/Hk2MWh7c/teto.gif',
+  'https://i.postimg.cc/YC3F2smN/vocaloidfan.gif',
+  'https://i.postimg.cc/cJLnvBQY/wanasuicie.png',
+]
+
+// ============ LED 滚动图片 ============
+export const scrollImages = [
+  'https://i.postimg.cc/0Qm2jzHV/ddg.gif',
+  'https://i.postimg.cc/SRfSLw4k/freespeechforever.gif',
+  'https://i.postimg.cc/br9y0KP2/getacomputer.jpg',
+  'https://i.postimg.cc/zvFzw9Nn/google-25wht.gif',
+  'https://i.postimg.cc/8kRzVr69/hpinewyear2006.gif',
+  'https://i.postimg.cc/7P3ZyTzk/html.gif',
+  'https://i.postimg.cc/KcP8ygTj/html-(2).gif',
+  'https://i.postimg.cc/Ghbh3hPY/internet-now02.gif',
+  'https://i.postimg.cc/vHGH8H7z/intnetfree.gif',
+  'https://i.postimg.cc/Yqtq2qNX/keep.gif',
+  'https://i.postimg.cc/8Pwk7Qw4/linuxnow.jpg',
+  'https://i.postimg.cc/X7xNrSxs/musicbug.gif',
+  'https://i.postimg.cc/dtnQhPnf/nop.gif',
+  'https://i.postimg.cc/Hs3WrDBm/schoolsucks.gif',
+  'https://i.postimg.cc/mgDLMX5G/site-best-viewed-with-monitor.gif',
+  'https://i.postimg.cc/5204v7RN/thedigitalme.gif',
+  'https://i.postimg.cc/g2JzRSTn/theoldnet-but.gif',
+  'https://i.postimg.cc/vZBY9qNL/thevoid.gif',
+  'https://i.postimg.cc/DwM2cr9L/tiredalien2.gif',
+  'https://i.postimg.cc/0yB8CYTC/tor.gif',
+  'https://i.postimg.cc/fRrwvcGH/vocaloid-now.gif',
+  'https://i.postimg.cc/Gm5cjFW6/webpeople.gif',
+  'https://i.postimg.cc/25LC9qYH/windose.png',
+  'https://i.postimg.cc/25LC9qmz/wwwbutton.gif',
+]
+
+// ============ 社交链接 ============
+export interface SocialLink {
+  name: string
+  url: string
+  img: string
+}
+
+export const socialLinks: SocialLink[] = [
+  {
+    name: 'email',
+    url: 'mailto:Eucaly<isntbunny@outlook.com>?subject=Hello%20^^&body=I%20found%20you%20at%20your%20website',
+    img: 'https://i.postimg.cc/c1hGm7Y8/email.gif',
+  },
+  {
+    name: 'github',
+    url: 'https://github.com/isntbunny',
+    img: 'https://i.postimg.cc/gjzFpr9h/github.gif',
+  },
+  {
+    name: 'twitter',
+    url: 'https:',
+    img: 'https://i.postimg.cc/Fzr5v1ts/Twitter.gif',
+  },
+  {
+    name: 'discord',
+    url: '#',
+    img: 'https://i.postimg.cc/fLjn1mPb/discord1.gif',
+  },
+]
+
+// ============ 技能 ============
+export interface SkillCategory {
+  title: string
+  items: (string | { name: string; icon: string })[]
+}
+
+export const skillCategories: SkillCategory[] = [
+  {
+    title: 'mastered',
+    items: [
+      {
+        name: 'HTML5',
+        icon: 'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
+      },
+      {
+        name: 'CSS3',
+        icon: 'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
+      },
+    ],
+  },
+  {
+    title: 'learning',
+    items: [
+      'English',
+      'Japanese',
+      {
+        name: 'Git',
+        icon: 'https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white',
+      },
+      {
+        name: 'TailwindCSS',
+        icon: 'https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white',
+      },
+      {
+        name: 'JavaScript',
+        icon: 'https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E',
+      },
+      {
+        name: 'Python',
+        icon: 'https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white',
+      },
+    ],
+  },
+  {
+    title: 'plan to learn',
+    items: [
+      {
+        name: 'Node.js',
+        icon: 'https://img.shields.io/badge/-Nodejs-43853d?style=flat-square&logo=Node.js&logoColor=white',
+      },
+      {
+        name: 'React',
+        icon: 'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
+      },
+      'making music',
+      'guitar',
+      'how to sew',
+    ],
+  },
+]
